1e6ef2301326f983ebb2d4e32bc333a110305d28
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-04-12 13:43:55 -0300

    Requerimiento 1

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public int cantTaxis;
+    public int cantDias;
+    public int cantDiasLluvia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sumaDias = cantDias + cantDiasLluvia;
+        int cantLitrosNormal = (cantTaxis * 90) * 130;
+        int cantLitrosLluvia = (cantTaxis * 110) * 130;
+        int sumaLitros = cantLitrosNormal + cantLitrosLluvia;
+        if(cantDias < 5)
+        {
+            Debug.Log("La cantidad de dias es menor a '5'");
+        }
+        else
+        {
+            Debug.Log("Una flota de "+ cantTaxis +" unidades trabajando durante "+ sumaDias +" días implicará un gasto de "+ sumaLitros +" pesos en concepto de combustible");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
